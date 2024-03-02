d8b5553188ef5ebf3737dd0d54cd39f972a1cb35
youkwhd <lolywk@tutanota.com>
2024-03-02 22:59:52 +0700

    chore: iterate path

--- src/main.zig
+++ src/main.zig
@@ -7,5 +7,32 @@ pub fn main() !void {
     var pathEnv = try std.process.getEnvVarOwned(allocator, "PATH");
     defer allocator.free(pathEnv);
 
-    try stdout.print("{s}\n", .{pathEnv});
+    var it = std.mem.splitScalar(u8, @as([]const u8, pathEnv), ':');
+
+    while (it.next()) |path| {
+        try stdout.print("{s}\n", .{path});
+
+        var dir = std.fs.openIterableDirAbsolute(path, .{}) catch {
+            continue;
+        };
+
+        defer dir.close();
+
+        var iterator = dir.iterate();
+
+        while (try iterator.next()) |entry| {
+            if (entry.kind == std.fs.File.Kind.file) {
+                var filepath = try std.fmt.allocPrint(allocator, "{s}/{s}", .{ path, entry.name });
+                defer allocator.free(filepath);
+
+                // bruh ain't no way i MUST open
+                // only for File.stat()
+                var file = try std.fs.openFileAbsolute(filepath, .{});
+                defer file.close();
+
+                var filestat = try file.stat();
+                _ = filestat;
+            }
+        }
+    }
 }
