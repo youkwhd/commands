7a94553a386060de4a9a116765c96b25d91e28b0
youkwhd <lolywk@tutanota.com>
2024-03-02 19:05:15 +0700

    feat: get the PATH env

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,11 @@
 const std = @import("std");
 
 pub fn main() !void {
+    const allocator = std.heap.page_allocator;
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("Hello from Zig\n", .{});
+
+    var pathEnv = try std.process.getEnvVarOwned(allocator, "PATH");
+    defer allocator.free(pathEnv);
+
+    try stdout.print("{s}\n", .{pathEnv});
 }
