0970e19b35971e1faf57db3c3aee782192fe7154
youkwhd <lolywk@tutanota.com>
2024-03-16 13:08:30 +0700

    fix: i forgot zig has c interop

--- build.zig
+++ build.zig
@@ -24,6 +24,10 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    // link libc (-lc) for
+    // <sys/stat.h>
+    exe.linkLibC();
+
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,15 @@
+const c = @cImport({
+    @cInclude("sys/stat.h");
+    @cInclude("unistd.h");
+});
+
 const std = @import("std");
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
     const stdout = std.io.getStdOut().writer();
+
+    var FILEPATH_MAX_LEN = 1024;
 
     var pathEnv = try std.process.getEnvVarOwned(allocator, "PATH");
     defer allocator.free(pathEnv);
@@ -10,8 +17,6 @@ pub fn main() !void {
     var it = std.mem.splitScalar(u8, @as([]const u8, pathEnv), ':');
 
     while (it.next()) |path| {
-        try stdout.print("{s}\n", .{path});
-
         var dir = std.fs.openIterableDirAbsolute(path, .{}) catch {
             continue;
         };
@@ -25,13 +30,17 @@ pub fn main() !void {
                 var filepath = try std.fmt.allocPrint(allocator, "{s}/{s}", .{ path, entry.name });
                 defer allocator.free(filepath);
 
-                // bruh ain't no way i MUST open
-                // only for File.stat()
-                var file = try std.fs.openFileAbsolute(filepath, .{});
-                defer file.close();
+                // TODO: alloc in heap
+                var filepath_with_null: [FILEPATH_MAX_LEN:0]u8 = undefined;
+                @memcpy(filepath_with_null[0..filepath.len], filepath);
+                filepath_with_null[filepath.len] = 0;
 
-                var filestat = try file.stat();
-                _ = filestat;
+                var fstat: c.struct_stat = undefined;
+                _ = c.stat(@as([*c]const u8, &filepath_with_null), &fstat);
+
+                if (fstat.st_mode & c.X_OK != 0) {
+                    try stdout.print("{s}\n", .{entry.name});
+                }
             }
         }
     }
