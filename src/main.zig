e8ee2c3c256a65266c6b6499b74902833c09d715
youkwhd <lolywk@tutanota.com>
2024-03-23 01:04:09 +0700

    chore: rename iterators

--- src/main.zig
+++ src/main.zig
@@ -13,33 +13,32 @@ pub fn main() !void {
     var pathEnv = try std.process.getEnvVarOwned(allocator, "PATH");
     defer allocator.free(pathEnv);
 
-    var it = std.mem.splitScalar(u8, @as([]const u8, pathEnv), ':');
-
-    while (it.next()) |path| {
+    var pathEnvIterator = std.mem.splitScalar(u8, @as([]const u8, pathEnv), ':');
+    while (pathEnvIterator.next()) |path| {
         var dir = std.fs.openIterableDirAbsolute(path, .{}) catch {
             continue;
         };
-
         defer dir.close();
 
-        var iterator = dir.iterate();
+        var dirIterator = dir.iterate();
+        while (try dirIterator.next()) |entry| {
+            if (entry.kind != std.fs.File.Kind.file) {
+                continue;
+            }
 
-        while (try iterator.next()) |entry| {
-            if (entry.kind == std.fs.File.Kind.file) {
-                var filepath = try std.fmt.allocPrint(allocator, "{s}/{s}", .{ path, entry.name });
-                defer allocator.free(filepath);
+            var filepath = try std.fmt.allocPrint(allocator, "{s}/{s}", .{ path, entry.name });
+            defer allocator.free(filepath);
 
-                // TODO: alloc in heap
-                var filepath_with_null: [FILEPATH_MAX_LEN:0]u8 = undefined;
-                @memcpy(filepath_with_null[0..filepath.len], filepath);
-                filepath_with_null[filepath.len] = 0;
+            // TODO: alloc in heap
+            var filepath_with_null: [FILEPATH_MAX_LEN:0]u8 = undefined;
+            @memcpy(filepath_with_null[0..filepath.len], filepath);
+            filepath_with_null[filepath.len] = 0;
 
-                var fstat: c.struct_stat = undefined;
-                _ = c.stat(@as([*c]const u8, &filepath_with_null), &fstat);
+            var fstat: c.struct_stat = undefined;
+            _ = c.stat(@as([*c]const u8, &filepath_with_null), &fstat);
 
-                if (fstat.st_mode & c.X_OK != 0) {
-                    try stdout.print("{s}\n", .{entry.name});
-                }
+            if (fstat.st_mode & c.X_OK != 0) {
+                try stdout.print("{s}\n", .{entry.name});
             }
         }
     }
